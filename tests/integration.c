287f5a1cc99ab609b59a616209dc04e61b8eaad3
bhadaura <bhadaura@student.ethz.ch>
2019-03-19 23:24:32 +0100

    ported objective functions to intregration testing

--- tests/integration.c
+++ tests/integration.c
@@ -20,6 +20,10 @@
 double sum_of_squares(const double* const args, size_t dim);
 double sum(const double* const args, size_t dim);
 double rastigrin(const double* const args, size_t dim);
+double rosenbrock(const double* const args, size_t dim){
+double sphere(const double* const args, size_t dim){
+double egghol2d(const double* const args, size_t dim){
+double schaf2d(const double* const args, size_t dim){
 
 
 /*******************************************************************************
@@ -68,6 +72,42 @@ double rastigrin(const double* const args, size_t dim) {
     rstgnND += pow(args[dim], 2.0) - 10 * cos(2.0 * M_PI * args[dim]);
   }
   return rstgnND;
+}
+
+/** Multidimensional Rosenbrock Function
+ global minima f(x1,.....,xN) = 0 at (x1,.....,xN) = (1,.....,1)*/
+double rosenbrock(const double* const args, size_t dim){
+  double rNd =0.;
+  for (size_t idx=0; idx<dim-1;idx++)
+      rNd += ( 100*(pow( x[idx+1] - pow(x[idx],2),2)) + pow(1-x[idx],2) );
+return rNd;
+}
+
+/* Multi Dimensional Sphere Function
+ global minima at f(x1,.....,xN) = 0 at (x1,......,xN) = (0,......,0)*/
+double sphere(const double* const args, size_t dim){
+  double sph = 0;
+  for (size_t idx=0; idx<dim ; idx++)
+      sph +=  pow(x[idx],2);
+return sph;
+}
+
+/**2D Eggholder Function
+	global minima at f(x,y) = -959.6407 at (x,y) = (512,404.2319)*/
+double egghol2d(const double* const args, size_t dim){
+  assert(dim == 2);
+  assert( fabs(x[0])<=512 && fabs(x[1])<=512 );
+  double eggh = -1*(x[1]+47)*sin(sqrt(fabs(x[0]+(x[1]+47))))
+                  		- x[0]*sin(sqrt(fabs(x[0]-(x[1]+47))));
+return eggh;
+}
+
+/** 2D Schaffer Function */
+double schaf2d(const double* const args, size_t dim){
+  assert(dim==2);
+  double sch = 0.5 + (pow(cos(pow(sin(fabs(x[0]*x[0]-x[1]*x[1])),2)),2) -0.5)/
+  										pow( (1+0.001*((x[0]*x[0]) + (x[1]*x[1]))), 2 );
+return sch;
 }
 
 
