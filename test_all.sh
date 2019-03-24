603434ec62401af9238c38e9ba95705b0d5967ef
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2019-03-24 12:15:40 +0100

    #7: started implementing hgwosca

--- test_all.sh
+++ test_all.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-declare -a arr=("penguin");
+declare -a arr=("penguin" "HGWOSCA");
 
 for d in "${arr[@]}"
 do
-    (cd "$d" && make)
+    (cd "$d" && make clean && make)
 done
 
 cd tests;
+make clean;
 make;
 make test;
