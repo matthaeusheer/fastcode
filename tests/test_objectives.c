edc3b516b9d68022f44cefea0b5c3bb3ab928cb5
Paulina <paudi18@gmail.com>
2019-04-08 14:18:13 +0200

    Update test_objectives.c
    adding the previous objective functions and extra ones

--- tests/test_objectives.c
+++ tests/test_objectives.c
@@ -1,8 +1,120 @@
 #include <float.h>
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "objectives.h"
 
 #include <criterion/criterion.h>
+
+#ifndef M_PI
+#define M_PI (3.14159265358979323846)
+#endif
+
+/*******************************************************************************
+  IMPLEMENTATIONS OF OBJECTIVE FUNCTIONS TO TEST ALGORITHMS
+******************************************************************************/
+
+/**
+ * Sum of squares function
+ * optimal solution is 0s everywhere
+ */
+double sum_of_squares(const double* const args, size_t dim) {
+    double sum = 0.0;
+    for(size_t idx = 0; idx < dim; idx++) {
+        sum += args[idx] * args[idx];
+    }
+    return sum;
+}
+
+/**
+ * Simple summation function
+ * optimal solution is 0s everywhere
+ */
+double sum(const double* const args, size_t dim) {
+    double sum = 0.0;
+    for(size_t idx = 0; idx < dim; idx++) {
+        sum += args[idx];
+    }
+    return sum;
+}
+
+/**
+ * Rastigrin function
+ * optimal solution is 0s everywhere
+ */
+double rastigrin(const double* const args, size_t dim) {
+    double rstgnND = 10 * dim;
+    for(size_t idx = 0; idx < dim; idx++) {
+        rstgnND += pow(args[dim], 2.0) - 10 * cos(2.0 * M_PI * args[dim]);
+    }
+    return rstgnND;
+}
+
+/**
+ * Multidimensional Rosenbrock Function
+ * global minima f(x1,.....,xN) = 0 at (x1,.....,xN) = (1,.....,1)
+ */
+double rosenbrock(const double* const args, size_t dim){
+    double rNd =0.;
+    for (size_t idx=0; idx<dim-1;idx++)
+        rNd += ( 100*(pow( args[idx+1] - pow(args[idx],2),2)) + pow(1-args[idx],2) );
+    return rNd;
+}
+
+/**
+ * Multi Dimensional Sphere Function
+ * global minima at f(x1,.....,xN) = 0 at (x1,......,xN) = (0,......,0)
+ */
+double sphere(const double* const args, size_t dim){
+    double sph = 0;
+    for (size_t idx=0; idx<dim ; idx++)
+        sph +=  pow(args[idx],2);
+    return sph;
+}
+
+//This class defines the Schwefel 1 global optimization problem. 
+//Has a global minima, similar to the sphere function, so is safe.
+//This is a unimodal minimization problem defined as follows:
+double schwefel01(const double* const args, size_t dim){
+    double sch1 = 0;
+    double alpha = sqrt(M_PI);
+    for (size_t idx=0; idx<dim; idx++)
+        sch1 += pow(pow(args[idx],2),alpha);
+    return sch1;   
+}
+
+/**
+ * 2D Eggholder Function
+ * global minima at f(x,y) = -959.6407 at (x,y) = (512,404.2319)
+ */
+double egghol2d(const double* const args, size_t dim){
+    //assert(dim == 2);
+    //assert( fabs(args[0])<=512 && fabs(args[1])<=512 );
+    double eggh = -1*(args[1]+47)*sin(sqrt(fabs(args[0]+(args[1]+47))))
+                  - args[0]*sin(sqrt(fabs(args[0]-(args[1]+47))));
+    return eggh;
+}
+
+/**
+ * 2D Schaffer Function
+ */
+double schaf2d(const double* const args, size_t dim){
+    double sch = 0.5 + (pow(cos(pow(sin(fabs(args[0]*args[0]-args[1]*args[1])),2)),2) -0.5)/
+                       pow( (1+0.001*((args[0]*args[0]) + (args[1]*args[1]))), 2 );
+    return sch;
+}
+
+/**
+ * Griewank function
+ */
+double griewank(const double* const args, size_t dim) {
+    double sum = 0, prod = 1;
+    for(size_t idx = 0; idx < dim; idx++) {
+        sum += pow(args[idx], 2) / 4000;
+        prod *= cos(args[idx] / sqrt(idx));
+    }
+    return sum - prod + 1;
+}
 
 Test(obj_unit, sum) {
     double args[] = {0, 1, 2, 3, 4, 5};
