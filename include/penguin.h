430d598d5e7ce56cba189dae64ded91c43ba04db
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2019-04-18 14:36:41 +0200

    tried some parameters out

--- include/penguin.h
+++ include/penguin.h
@@ -14,18 +14,18 @@ extern "C" {
 // attenuation coefficient
 // should be above 1.0 at all times in order to not revert the exponent in equation 11
 #define ATT_COEF_START 1.0
-#define ATT_COEF_STEP 0.001
+#define ATT_COEF_STEP 0
 
 // heat absorption coefficient
 #define HAB_COEF_START 1
-#define HAB_COEF_STEP 0.001
+#define HAB_COEF_STEP 0
 
 // mutation coefficient
-#define MUT_COEF_START 0.2
-#define MUT_COEF_STEP 0.001
+#define MUT_COEF_START 0.02
+#define MUT_COEF_STEP 0
 
-#define A 3       // see equ 18 from paper
-#define B 0.1     // see equ 18 from paper
+#define A 0.1       // see equ 18 from paper
+#define B 1     // see equ 18 from paper
 
 
 // helper utilities
