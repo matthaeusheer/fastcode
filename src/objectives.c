0d33670de72c0668896a3733d1083c361c6a5877
Paulina <paudi18@gmail.com>
2019-04-25 22:23:30 +0200

    Update objectives.c

--- src/objectives.c
+++ src/objectives.c
@@ -24,6 +24,21 @@ double sum_of_squares(const double *const args, size_t dim) {
 }
 
 /**
+* Sum of cosines
+* will always converge
+*/ 
+double cosinemixture(const double *cost args, size_t dim){
+  double cosmix = 0.0;
+  double cosmix1 = 0.0;
+  for(size_t idx = 0; idx < dim; idx++) {
+     cosmix += - args[idx] * args[idx];
+     cosmix1 += - 0.1*cos(5*M_PI*args[idx]); 
+  }
+  return cosmix + cosmix1;
+}
+
+
+/**
  * Simple summation function
  * optimal solution is 0s everywhere
  */
@@ -66,6 +81,28 @@ double rosenbrock_neg(const double *const args, size_t dim) {
   return -1.0 * rosenbrock(args, dim);
 }
 
+
+/**
+41.58%, N=2
+This class defines the Freudenstein & Roth global 
+optimization problem. This is a multimodal minimization problem defined as follows:
+*/
+double freundsteinroth(const double *const args, size_t dim) {
+  double fr = pow((args[0] - 13 + ((5 - args[1]) * args[1] - 2) * args[1]), 2)
+              + pow((args[0] - 29 + ((args[1] + 1) * args[1] - 14) * args[1]), 2);
+  return fr;
+}
+
+
+/**
+This class defines the Powell global optimization problem. 	N=4,	32.58%
+This is a multimodal minimization problem defined as follows
+  */
+double Powel(const double *const args, size_t dim){
+  double powel = pow((arg[2] + 10 * arg[0]), 2) + 5 * pow((arg[1] - arg[3]), 2) + pow((arg[0] - 2 * arg[1]), 4) + 10 * pow((arg[2] - arg[3]), 4) ;
+  return powel;
+}
+
 /**
  * Multi Dimensional Sphere Function
  * global minima at f(x1,.....,xN) = 0 at (x1,......,xN) = (0,......,0)
@@ -76,6 +113,20 @@ double sphere(const double *const args, size_t dim) {
     sph += pow(args[idx], 2);
   return sph;
 }
+
+/**This class defines the Schwefel 1 global optimization problem. 
+This is a unimodal minimization problem defined as follows:
+
+*/
+double schwefel01(const double *const args, size_t dim){
+  double sch00 = 0;
+  double alpha = sqrt(M_PI)
+  for (size_t idx = 0; idx < dim; idx++) {
+    sch00 += pow(args[idx],2);
+  }
+  return pow(sch00, alpha);
+}  
+
 
 /**
  * 2D Eggholder Function
@@ -109,3 +160,17 @@ double griewank(const double *const args, size_t dim) {
   }
   return sum - prod + 1;
 }
+
+/**
+*Beale 
+This class defines the Beale global optimization problem. This is a multimodal minimization problem defined as follows:
+*/
+double beale(const double *const args, size_t dim) {
+double beal = pow((args[0] * args[2] - args[0] + 1.5), 2) + pow((args[0] * pow(arg[1], 2)), 2) - args[0] + 2.25), 2)
+              + pow((pow(args[0] * args[1], 3) - args[0] + 2.625), 2);
+return beal;  
+}
+
+
+
+
