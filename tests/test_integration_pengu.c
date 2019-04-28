9c8566af7b86d5ae007497acfd18248b2b668de0
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2019-04-28 16:02:40 +0200

    #34 added small integration tests pengu

--- tests/test_integration_pengu.c
+++ tests/test_integration_pengu.c
@@ -5,37 +5,38 @@
 #include "penguin.h"
 #include "utils.h"
 
+#include <stdlib.h>
+#include <stdio.h>
 
-Test(pengu_integration, sum) {
-  size_t population_size = 30;
+
+Test(pengu_integration, sum_of_squares) {
+  size_t population_size = 50;
   size_t dim = 2;
   size_t max_iter = 100;
-  double* maxs = filled_double_array(dim, 100);
-  double* mins = filled_double_array(dim, -100);
-  double* solution = pen_emperor_penguin(sum, population_size, dim, max_iter, mins, maxs);
+  double* mins = filled_double_array(dim, -5);
+  double* maxs = filled_double_array(dim, 5);
+  double* solution = pen_emperor_penguin(sum_of_squares, population_size, dim, max_iter, mins, maxs);
   for(size_t idx = 0; idx < dim; idx++) {
-    cr_assert(fabs(solution[idx] + 100) < 0.00001, "each dimension should be -100");
+    cr_assert(fabs(solution[idx]) < 0.1, "each dimension should be zero");
   }
-  cr_assert(fabs(sum(solution, dim) + 100 * dim) < 0.2 * dim, "objective should be "
-                                                              "minimised at -100*dim");
+  cr_assert(fabs(sum_of_squares(solution, dim)) < 0.1, "objective should be minimised at 0");
   free(solution);
   free(mins);
   free(maxs);
 }
 
 
-Test(pengu_integration, sum_of_squares) {
-  size_t population_size = 30;
+Test(pengu_integration, rosenbrock) {
+  size_t population_size = 50;
   size_t dim = 2;
-  size_t max_iter = 120;
-  double* mins = filled_double_array(dim, -100);
-  double* maxs = filled_double_array(dim, 100);
-  double* solution = pen_emperor_penguin(sum_of_squares, population_size, dim, max_iter, mins, maxs);
-  print_solution(dim, solution);
+  size_t max_iter = 100;
+  double* mins = filled_double_array(dim, -5);
+  double* maxs = filled_double_array(dim, 5);
+  double* solution = pen_emperor_penguin(rosenbrock, population_size, dim, max_iter, mins, maxs);
   for(size_t idx = 0; idx < dim; idx++) {
-    cr_assert(fabs(solution[idx]) < 0.00001, "each dimension should be zero");
+    cr_assert(fabs(solution[idx]) - 1.0 < 0.1, "each dimension should be one");
   }
-  cr_assert(fabs(sum_of_squares(solution, dim)) < 0.1, "objective should be minimised at 0");
+  cr_assert(fabs(sum_of_squares(solution, dim)) < 3, "objective should be minimised at 0");
   free(solution);
   free(mins);
   free(maxs);
