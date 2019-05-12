80dde4af185f8628f0514d284c08fd9e6868f94c
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2019-05-12 13:37:16 +0200

    #71 improved integration test output on travis to provide more details

--- tests/test_integration_pengu.c
+++ tests/test_integration_pengu.c
@@ -6,21 +6,26 @@
 
 
 Test(pengu_integration, sum) {
-  test_algo(sum, 50, 2, -5, 5, 100, pen_emperor_penguin, -10, 0.1, false);
+  test_algo(sum, 50, 2, -5, 5, 100, pen_emperor_penguin, -10, 0.1,
+            true, "EPC", "sum");
 }
 
 Test(pengu_integration, sum_of_squares) {
-  test_algo(sum_of_squares, 50, 2, -5, 5, 100, pen_emperor_penguin, 0, 0.1, false);
+  test_algo(sum_of_squares, 50, 2, -5, 5, 100, pen_emperor_penguin, 0, 0.1,
+            true, "EPC", "sum_of_squares");
 }
 
 Test(pengu_integration, rosenbrock) {
-  test_algo(rosenbrock, 100, 2, -5, 5, 200, pen_emperor_penguin, 0, 0.01, false);
+  test_algo(rosenbrock, 100, 2, -5, 5, 300, pen_emperor_penguin, 0, 0.3,
+            true, "EPC", "rosenbrock");
 }
 
 Test(pengu_integration, rastigrin) {
-  test_algo(rastigrin, 100, 2, -5, 5, 2000, pen_emperor_penguin, 0, 0.1, false);
+  test_algo(rastigrin, 100, 2, -5, 5, 2000, pen_emperor_penguin, 0, 0.1,
+            true, "EPC", "rastigrin");
 }
 
 Test(pengu_integration, sphere) {
-  test_algo(sphere, 100, 2, -5, 5, 200, pen_emperor_penguin, 0, 0.1, false);
+  test_algo(sphere, 100, 2, -5, 5, 200, pen_emperor_penguin, 0, 0.1,
+            true, "EPC", "sphere");
 }
--- tests/test_integration_pso.c
+++ tests/test_integration_pso.c
@@ -8,14 +8,17 @@
 #define DIM 2
 
 
-Test(pso_basic_integration, sum_of_squares) {
-  test_algo(sum_of_squares, SWARM_SIZE, DIM, -10, 10, 1000, pso_basic, 0, 0.1, false);
+Test(pso_integration, sum_of_squares) {
+  test_algo(sum_of_squares, SWARM_SIZE, DIM, -10, 10, 1000, pso_basic, 0, 0.1,
+            true, "PSO", "sum_of_squares");
 }
 
-Test(pso_basic_integration, sphere) {
-  test_algo(sphere, SWARM_SIZE, DIM, -10, 10, 1000, pso_basic, 0, 0.1, false);
+Test(pso_integration, sphere) {
+  test_algo(sphere, SWARM_SIZE, DIM, -10, 10, 1000, pso_basic, 0, 0.1,
+            true, "PSO", "sphere");
 }
 
 Test(pso_integration, rosenbrock) {
-  test_algo(rosenbrock, SWARM_SIZE, DIM, -30, 30, 1000, pso_basic, 0, 0.01, false);
+  test_algo(rosenbrock, SWARM_SIZE, DIM, -30, 30, 1000, pso_basic, 0, 0.01,
+            true, "PSO", "rosenbrock");
 }
