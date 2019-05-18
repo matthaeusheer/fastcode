02fbe3200bea77307f3e6f50f67aa16d93c54a20
Matthäus Heer <maheer@student.ethz.ch>
2019-05-18 13:03:23 +0200

    Method to calculate operational intensity.

--- fastpy/fastpy/evaluation/performance_calculations.py
+++ fastpy/fastpy/evaluation/performance_calculations.py
@@ -1,10 +1,9 @@
 from typing import Dict
 
 
-class FlopCounter:
+class PerfCounter:
     def __init__(self, run_config: Dict) -> None:
-        """Calculates the flop count for a particular execution of an optimisation algorithm with a given set
-        of parameters specified in the run_config (obj_func, iterations, dimensions, ...).
+        """Base class for FlopCounter and MemmoryMoveCounter.
 
         Arguments
         ---------
@@ -32,6 +31,58 @@ class FlopCounter:
     def algorithm(self):
         return self.config['algorithm']
 
+
+def calc_op_intensity(run_config: Dict) -> float:
+    """Given a run configuration, what is the operational intensity (FLOPS / BYTES)."""
+    mem_move_bytes = MemoryMoveCounter(run_config).mem_movement_bytes()
+    total_flop_cont = FlopCounter(run_config).flop_count()
+
+    return total_flop_cont / mem_move_bytes
+
+
+class MemoryMoveCounter(PerfCounter):
+    def __init__(self, run_config: Dict) -> None:
+        """Calculates memory movement for a particular execution of an optimisation algorithm with a given set
+        of parameters specified in the run_config (obj_func, iterations, dimensions, ...).
+
+        Arguments
+        ---------
+            run_config: dict with algorithm parameter and corresponding value
+        """
+        super().__init__(run_config)
+
+    def mem_movement_bytes(self) -> int:
+        dim = self.dimension
+        pop = self.pop_size
+        n_iter = self.n_iter
+
+        if self.algorithm == 'pso':
+            return 16 + 12 * dim * pop + 8 * pop + 4 * (n_iter + dim)
+        else:
+            raise NotImplementedError(f'No memory usage function for {self.algorithm} yet...')
+
+    def mem_movement_floats(self) -> int:
+        dim = self.dimension
+        pop = self.pop_size
+        n_iter = self.n_iter
+
+        if self.algorithm == 'pso':
+            return 4 + 3 * dim * pop + 2 * pop + n_iter + dim
+        else:
+            raise NotImplementedError(f'No memory usage function for {self.algorithm} yet...')
+
+
+class FlopCounter(PerfCounter):
+    def __init__(self, run_config: Dict) -> None:
+        """Calculates the flop count for a particular execution of an optimisation algorithm with a given set
+        of parameters specified in the run_config (obj_func, iterations, dimensions, ...).
+
+        Arguments
+        ---------
+            run_config: dict with algorithm parameter and corresponding value
+        """
+        super().__init__(run_config)
+
     def obj_func_flops(self) -> int:
         if self.obj_func == 'sum_of_squares':
             return self.dimension * 2
@@ -52,6 +103,7 @@ class FlopCounter:
             return self.dimension * 6
 
     def flop_count(self) -> int:
+        """Calculates total flop counts for different algorithms depending on the run configuration."""
         # for better readability inside the expressions, have local variables
         pop_size = self.pop_size
         dimension = self.dimension
@@ -80,5 +132,7 @@ class FlopCounter:
             ])
             return flop_count
 
+        elif self.algorithm == 'pso':
+            pass
         else:
             raise NotImplementedError(f'Algorithm {self.algorithm} not implemented yet for performance plot.')
