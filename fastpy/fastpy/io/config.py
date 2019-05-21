27e8ca5bdca04eeff4386816e4bcdc20f51e10b6
matthaeusheer <maheer@student.ethz.ch>
2019-05-21 12:10:39 +0200

    Range based parameter config.

--- fastpy/fastpy/io/config.py
+++ fastpy/fastpy/io/config.py
@@ -4,7 +4,18 @@ import os
 
 def load_json_config(config_path):
     with open(config_path, 'r') as infile:
-        return json.load(infile)
+        conf = json.load(infile)
+        for key, item in conf.items():
+            if isinstance(item, list):
+                pass
+            elif isinstance(item, str):
+                if item.startswith('range'):
+                    print(key)
+                    start, stop, step = [int(val) for val in item[item.find("(") + 1:item.find(")")].split(',')]
+                    conf[key] = list(range(start, stop, step))
+                else:
+                    raise (ValueError('String in config only allowed as "range(start, stop, step)".'))
+        return conf
 
 
 def store_json_config(config, dir_path, file_name):
