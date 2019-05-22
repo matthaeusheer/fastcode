58492f9cda19c576db94345afb390d11ec9a31e0
matthaeusheer <maheer@student.ethz.ch>
2019-05-22 17:02:10 +0200

    Added warning to incomplete script.

--- build-releases.sh
+++ build-releases.sh
@@ -3,11 +3,14 @@
 # This is a shell script to create executables for different releases in a folder called build-releases.
 # The executables will be named benchmark_<version-tag>
 
-# CAREFUL: This is a very rough script, if the git checkout fails (e.g. unknown release) it keeps running
+##################################### CAREFUL: ###################################################
+# This is a very rough script for convenience, if the git checkout fails (e.g. unknown release) it keeps running
 # on the current branch and might compile the same release executable all over again.
+# The checkout also does not work if you have un-commited changes on your branch.
+##################################################################################################
 
 build_releases_dir="build-releases"
-declare -a  tags=("pso0.0.1" "pso0.0.2" "pso0.0.3" "pso0.0.4" "pso0.0.5" "pso0.0.6")
+declare -a  tags=("v0.0.1" "pso0.0.1" "pso0.0.2" "pso0.0.3" "pso0.0.4" "pso0.0.5" "pso0.0.6")
 
 original_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
