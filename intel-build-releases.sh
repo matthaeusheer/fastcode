dc3c2d4cb729b6170d101fa64ff68547ae71397b
Matthäus Heer <maheer@student.ethz.ch>
2019-05-24 14:12:18 +0200

    Updated intel build release files.

--- intel-build-releases.sh
+++ intel-build-releases.sh
@@ -10,7 +10,7 @@
 ##################################################################################################
 
 build_releases_dir="build-releases"
-declare -a  tags=("base" "pso0.0.1" "pso0.0.2" "pso0.0.3" "pso0.0.4" "pso0.0.5" "pso0.0.6" "pso0.0.7")
+declare -a  tags=("doubles" "base" "pso0.0.1" "pso0.0.2" "pso0.0.3" "pso0.0.4" "pso0.0.5" "pso0.0.6" "pso0.0.7" "pso0.0.8")
 
 original_branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
 
