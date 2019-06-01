eb86ae2597b628d1a73d6ba66430fffc652a07e8
Abhimanyu <bhadaura@student.ethz.ch>
2019-06-01 11:45:20 +0200

    update plot script to increase input size in powers of 2's

--- fastpy/fastpy/io/config.py
+++ fastpy/fastpy/io/config.py
@@ -12,7 +12,8 @@ def load_json_config(config_path, is_sub_config=False):
                 elif isinstance(item, str) and item not in []:
                     if item.startswith('range'):
                         start, stop, step = [int(val) for val in item[item.find("(") + 1:item.find(")")].split(',')]
-                        conf[key] = list(range(start, stop, step))
+                        # conf[key] = list(range(start, stop, step))
+                        conf[key] = [2**val for val in range(start,stop,step)]
                     else:
                         raise (ValueError('String in config only allowed as "range(start, stop, step)".'))
         return conf
@@ -21,3 +22,4 @@ def load_json_config(config_path, is_sub_config=False):
 def store_json_config(config, dir_path, file_name):
     with open(os.path.join(dir_path, file_name), 'w') as outfile:
         json.dump(config, outfile, indent=4)
+
